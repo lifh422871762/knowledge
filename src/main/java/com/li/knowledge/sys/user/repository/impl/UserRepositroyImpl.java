da0c022c12660acf3f064998a7f9760cc6aa4d44
lifuhao <12345678>
2020-08-11 22:06:09 +0800

    添加查询条件

--- src/main/java/com/li/knowledge/sys/user/repository/impl/UserRepositroyImpl.java
+++ src/main/java/com/li/knowledge/sys/user/repository/impl/UserRepositroyImpl.java
@@ -5,6 +5,7 @@ import com.alibaba.druid.util.JdbcConstants;
 import com.li.knowledge.sys.user.model.dto.UserDTO;
 import com.li.knowledge.sys.user.model.vo.UserVO;
 import com.li.knowledge.sys.user.repository.UserRepositroyCustomr;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -51,9 +52,14 @@ public class UserRepositroyImpl implements UserRepositroyCustomr {
         String sql = "";
         //查询条件在这里拼接
         if (null != userDTO) {
-
+            if(StringUtils.isNotBlank(userDTO.getLoginName())){
+                sql += " and login_name = :loginName";
+            }
+            if(0 != userDTO.getStatus()){
+                sql += " and status = :status";
+            }
         }
-        sql = sql + " ORDER BY lu.modified_at desc";//更新时间倒叙
+        sql = sql + " ORDER BY lu.seq asc";//排序字段排序
         return sql;
     }
 }
